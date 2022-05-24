57aa1134249ab0ab638b86f34a7f5a6c871c141b
Karan Ganesan <karan.ganesan@flipkart.com>
2022-05-24 15:39:38 +0530

    fix: scroll to end

--- src/App.svelte
+++ src/App.svelte
@@ -31,6 +31,7 @@
               .getElementById(file.name)
               .toDataURL("image/png", 1);
             downloadURI(image, file.name.split(".")[0] + "_back.png");
+            window.scrollTo(0, document.body.scrollHeight);
           }, 2000);
         },
         { once: true }
